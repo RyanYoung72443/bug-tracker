3e2354109152702b34629fec70f95cf5f24472ac
ShawnYoung72443 <sryoung72443@gmail.com>
2020-09-20 01:27:37 -0400

    basic updates to load logs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layouts/SearchBar';
+import Logs from './components/logs/Logs';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -8,9 +11,12 @@ function App() {
     M.AutoInit();
   })
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
