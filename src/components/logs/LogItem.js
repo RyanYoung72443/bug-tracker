1465651bacb31497ea184a4befd940e209e53c9e
ShawnYoung72443 <sryoung72443@gmail.com>
2020-09-20 22:01:12 -0400

    fixed fails and added delete for logs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,24 +1,34 @@
-import React from 'react'
-import Moment from 'react-moment'
-import PropTypes from 'prop-types'
+import React from 'react';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+import { deleteLog } from '../../actions/logActions';
+import PropTypes from 'prop-types';
 
-const LogItem = ({ log }) => {
+import M from 'materialize-css/dist/js/materialize.min.js'
+
+const LogItem = ({ log: { attention, message, id, tech, date }, deleteLog }) => {
+
+  const onDelete = () => {
+    deleteLog(id);
+    M.toast({ html: 'Log Deleted' });
+  }
+
   return (
     <li className="collection-item">
       <div>
         <a
           href='#edit-log-modal'
-          className={`modal-trigger ${log.attention ? 'red-text' : 'green-text'}`}>
-          {log.message}
+          className={`modal-trigger ${attention ? 'red-text' : 'green-text'}`}>
+          {message}
         </a>
         <br />
         <span className="grey-text">
-          <span className="black-text">ID #{log.id}</span> last updated by{' '}
-          <span className="black-text">{log.tech}</span> on {' '}
-          <Moment format='MMMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
+          <span className="black-text">ID #{id}</span> last updated by{' '}
+          <span className="black-text">{tech}</span> on {' '}
+          <Moment format='MMMM Do YYYY, h:mm:ss a'>{date}</Moment>
         </span>
         <a href="#!" className="secondary-content">
-          <i className="material-icons grey-text">delete</i>
+          <i className="material-icons grey-text" onClick={onDelete}>delete</i>
         </a>
       </div>
     </li>
@@ -27,6 +37,7 @@ const LogItem = ({ log }) => {
 
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired,
 }
 
-export default LogItem
+export default connect(null, { deleteLog })(LogItem)
